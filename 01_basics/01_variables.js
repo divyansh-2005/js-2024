33724757ca71b126b83e3d24387f68cd415c115d
Divyansh Kumbhat <divyanshkumbhat@gmail.com>
2024-01-03 17:47:03 +0530

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,16 @@
+const accountId = 15446
+let accountEmail = "divyansh@gmail.com"
+var accountPassword = "12345"
+accountCity = "Nagpur"
+
+let accountState;
+
+//accountId = 2 // not allowed const variable
+
+accountEmail = "dev@gmail.com"
+accountPassword ="54321"
+accountCity = "jodhpur"
+
+//prefer not to use var
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,24 @@
+"use strict"; //treat all JS code as newrt version
+
+//alert(3+3) // we are using nodejs, not browser
+
+console.log("Divyansh");
+
+let name = "divya"
+let age = 18
+let isLoggedIn = false
+/*
+number => 2^53
+BigInt
+String => ""
+Boolean => true/false
+null => standalone value
+undifined 
+symbol => unique
+*/
+
+// object
+
+console.log(typeof name);
+console.log(typeof null);
+console.log(typeof undefined);
