423ffa2bff3b5dd78c5a8e67a3ec61a2e70b27e5
Divyansh Kumbhat <divyanshkumbhat@gmail.com>
2024-01-03 19:56:29 +0530

    addtion in datatype-summary.js

--- 01_basics/datatypes-summary.js
+++ 01_basics/datatypes-summary.js
@@ -14,7 +14,7 @@ let userEmail;
 
 const id = Symbol('123')
 const anotherId = Symbol('123')
-console.log(id === anotherId);
+//console.log(id === anotherId);
 
 const bigNumber = 35465435464643213264n
 
@@ -30,10 +30,32 @@ let myObj={
 }
 
 const myFunction = function(){
-    console.log("Hello world");
+    // console.log("Hello world");
 }
 
-console.log(typeof bigNumber);
-console.log(typeof outsideTemp);
-console.log(typeof myFunction);
-console.log(typeof heros);
+// console.log(typeof bigNumber);
+// console.log(typeof outsideTemp);
+// console.log(typeof myFunction);
+// console.log(typeof heros);
+
+//++++++++++++++++++++++++++++++++++++++++++++++++++
+// Stack(primitive), heap(Non-Primitive)
+
+//stack
+let name = "divya"
+let anothername = name
+anothername = "divyansh"
+console.log(name);
+console.log(anothername);
+
+//Heap
+let user1={
+    email: "user@google.com",
+    upi: "user@ybl"
+}
+
+let user2=user1
+
+user2.email = "divya@google.com"
+console.log(user1.email);
+console.log(user2.email);
--- 01_basics/tempCodeRunnerFile.js
+++ 01_basics/tempCodeRunnerFile.js
@@ -1,6 +1,1 @@
-console.log(str3);
-
-// console.log("1" + 2);
-// console.log(1 + "2");
-// console.log("1"+2+2);
-// console.log(1+2+"2");
+ // console.log("Hello world");
