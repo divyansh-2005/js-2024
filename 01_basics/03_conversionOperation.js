829d16cb6e5b02bbcdb9ef628642ad3c009c59f6
Divyansh Kumbhat <divyanshkumbhat@gmail.com>
2024-01-03 19:21:09 +0530

    operations

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,15 +1,15 @@
 let score = "33abc"
-console.log(typeof score);
-console.log(typeof(score));
+// console.log(typeof score);
+// console.log(typeof(score));
 
 let valueInNumber = Number(score)
-console.log(typeof valueInNumber);
-console.log(valueInNumber);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
 
 score = null
 valueInNumber = Number(score)
-console.log(typeof valueInNumber);
-console.log(valueInNumber);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
 
 /*
 "33" => 33
@@ -19,7 +19,7 @@ true => 1; false => 0
 
 let isLoggedIn = 1
 let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn);
+//console.log(booleanIsLoggedIn);
 
 // 1=>true
 // "" => false
@@ -28,5 +28,31 @@ console.log(booleanIsLoggedIn);
 let somenumber = 33
 
 let stringNumber = String(somenumber)
-console.log(stringNumber)
-console.log(typeof stringNumber);
+// console.log(stringNumber)
+// console.log(typeof stringNumber);
+
+//********** OPERATIONS **********
+let value = 3
+let negValue = -value
+//console.log(negValue);
+
+let str1 = "hello"
+let str2 = "divya"
+
+let str3 = str1+str2
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1"+2+2);
+// console.log(1+2+"2");
+
+// console.log(+true); 
+// console.log(+"");
+
+let num1,num2,num3
+num1 = num2 = num3 = 2+2
+
+let gameCounter = 100
+gameCounter++
+console.log(gameCounter);
---
+++ 01_basics/tempCodeRunnerFile.js
@@ -0,0 +1,6 @@
+console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1"+2+2);
+// console.log(1+2+"2");
