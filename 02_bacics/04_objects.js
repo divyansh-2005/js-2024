6052d245cdf387a291154c8b780551f6f8f48b18
Divyansh Kumbhat <divyanshkumbhat@gmail.com>
2024-01-04 22:10:51 +0530

    Object de-structuring

--- 02_bacics/04_objects.js
+++ 02_bacics/04_objects.js
@@ -45,10 +45,25 @@ const users = [
 ]
 
 users[1].email
-console.log(tinderUser)
+// console.log(tinderUser)
 
-console.log(Object.keys(tinderUser));
-console.log(Object.values(tinderUser));
-console.log(Object.entries(tinderUser));
+// console.log(Object.keys(tinderUser));
+// console.log(Object.values(tinderUser));
+// console.log(Object.entries(tinderUser));
 
-console.log(tinderUser.hasOwnProperty('isLogeed'));
+// console.log(tinderUser.hasOwnProperty('isLogeed'));
+
+
+const course = {
+    coursename: "js in hindi", 
+    price: "999",
+    courseInstructor:"hitesh"
+}
+
+// course.courseInstructor
+//de-structuring
+const {courseInstructor:instructor} = course
+
+// console.log(courseInstructor);
+console.log(instructor);
+f
