986712325809e9eada4783d0280fb46cceeff75e
Divyansh Kumbhat <divyanshkumbhat@gmail.com>
2024-01-14 09:43:48 +0530

    Getter Setter

---
+++ 10_classes_and_oop/properties_get_set.js
@@ -0,0 +1,26 @@
+function User(email, password){
+    this._email = email
+    this._password = password
+
+    Object.defineProperty(this, 'email', {
+        get: function(){
+            return this._email.toUpperCase()
+        },
+        set: function(value){
+            this._email = value
+        }
+    })
+
+    Object.defineProperty(this, 'password', {
+        get: function(){
+            return this._password.toUpperCase()
+        },
+        set: function(value){
+            this._password = value
+        }
+    })
+}
+
+const divya = new User("divya@gmail.com", "abc")
+console.log(divya.password);
+console.log(divya.email);
